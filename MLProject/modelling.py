508a6bca192523b1ab5a621a0e6c144145d83f12
CutRifa <cutrifas@gmail.com>
2025-12-21 23:25:08 +0700

    Update project to use Stellar Classification dataset

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,37 +2,28 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn import datasets
-import matplotlib.pyplot as plt
-import seaborn as sns
 import os
 
-# 1. Load Data Iris
-print("[INFO] Loading data Iris...")
-iris = datasets.load_iris()
-X = pd.DataFrame(iris.data, columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
-y = iris.target
-
-# Split Data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# 1. Load Data dari folder preprocessed
+data_path = "stellar_preprocessed"
+X_train = pd.read_csv(f"{data_path}/X_train.csv")
+y_train = pd.read_csv(f"{data_path}/y_train.csv")
+X_test = pd.read_csv(f"{data_path}/X_test.csv")
+y_test = pd.read_csv(f"{data_path}/y_test.csv")
 
 # 2. Setup MLflow
-mlflow.set_experiment("Eksperimen_Iris_Rifa_CI")
+mlflow.set_experiment("Stellar_Classification_Project")
 
-# 3. Training
 with mlflow.start_run():
-    rf = RandomForestClassifier(n_estimators=100, random_state=42)
-    rf.fit(X_train, y_train)
+    # Training
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train.values.ravel())
     
-    # Prediksi
-    y_pred = rf.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print(f"[INFO] Accuracy: {acc:.4f}")
+    # Eval
+    acc = accuracy_score(y_test, model.predict(X_test))
+    print(f"Accuracy: {acc}")
     
-    # Logging
+    # Log Artifacts
     mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(rf, "model_iris") # Nama folder model
-    
-    print("[SUCCESS] Training Selesai.")
+    mlflow.sklearn.log_model(model, "model_bintang")
