685799e8537d406d73fbdff15dd5dd168a9ce889
CutRifa <cutrifas@gmail.com>
2025-12-22 12:31:29 +0700

    Fix: Final path alignment and env-manager local

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,6 +5,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import os
 
+# --- KRITERIA 2: Aktifkan Logging Otomatis ---
+mlflow.autolog()
+
 # 1. Load Data dari folder preprocessed
 data_path = "stellar_preprocessed"
 X_train = pd.read_csv(f"{data_path}/X_train.csv")
@@ -12,18 +15,11 @@ y_train = pd.read_csv(f"{data_path}/y_train.csv")
 X_test = pd.read_csv(f"{data_path}/X_test.csv")
 y_test = pd.read_csv(f"{data_path}/y_test.csv")
 
-# 2. Setup MLflow
-mlflow.set_experiment("Stellar_Classification_Project")
+# 3. Training
+# Parameter dan metrik akan dicatat otomatis oleh autolog
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train.values.ravel())
 
-with mlflow.start_run():
-    # Training
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train.values.ravel())
-    
-    # Eval
-    acc = accuracy_score(y_test, model.predict(X_test))
-    print(f"Accuracy: {acc}")
-    
-    # Log Artifacts
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model_bintang")
+# 4. Evaluation
+acc = accuracy_score(y_test, model.predict(X_test))
+print(f"Accuracy: {acc}")
